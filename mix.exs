d2a42f94d3a67a990d0b4da0a21ffc675f7aec3b
Silviu Rosu <silviu.rosu@gmail.com>
2017-01-15 15:42:39 +0200

    add credo for code styling

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Admin.Mixfile do
   def project do
     [app: :admin,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
@@ -37,7 +37,8 @@ defmodule Admin.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:credo, "~> 0.5", only: [:dev, :test]}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
